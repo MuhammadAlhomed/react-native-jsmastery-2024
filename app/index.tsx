4b78a8a7592ca6eb2c392450439665537d39399f
Muhammad Alhomed <m.alhumed@qu.edu.sa>
2024-11-12 14:41:26 +0300

    test 'profile' page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={style.container}>
           <Text>Index</Text>
-          <StatusBar style='auto'/>
+          <Link href='/profile' style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   )
 }
