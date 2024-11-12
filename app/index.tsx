4a6f811887adec4afb22ce68c8d91892d8fd9008
Muhammad Alhomed <m.alhumed@qu.edu.sa>
2024-11-12 12:53:31 +0300

    create index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function Index() {
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={style.container}>
+          <Text>Index</Text>
+          <StatusBar style='auto'/>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
